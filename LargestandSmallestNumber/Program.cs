aeb16e798cf6f3d97857eb394265a1ed0d52b0ec
Monisha Khapali <monisha.khapali@logicsolutions.com>
2020-10-30 12:43:43 -0400

    Added

---
+++ LargestandSmallestNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LargestandSmallestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            /*find the largest and smallest number in an unsorted integer array*/
+            int[] a = { 1, 10, 12, 3, 4 };
+            FindLargestSmallestElement(a);
+        }
+        public static void FindLargestSmallestElement(int[] a)
+        {
+            int max = a[0];
+            int min = a[0];
+            for(int i=0; i<a.Length;i++)
+            {
+                if(a[i] < min)
+                {
+                    min = a[i];
+                }
+                if(a[i] > max)
+                {
+                    max = a[i];
+                }
+            }
+            Console.WriteLine(min +" "+ max);
+            Console.WriteLine();
+
+        }
+    }
+}
